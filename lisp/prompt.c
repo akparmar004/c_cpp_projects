a9f563acd9a691cf2e95094c603a0e207d6d62c2
akparmar004 <akparmar0404@gmail.com>
2024-04-12 21:19:57 +0530

    added one more parser

--- lisp/prompt.c
+++ lisp/prompt.c
@@ -32,6 +32,22 @@ void add_history(char *unused) {}
 
 int main(int argc,char **argv)
 {
+
+	mpc_parser_t* Number = mpc_new("number");
+	mpc_parser_t* Operator = mpc_new("operator");
+	mpc_parser_t* Expr = mpc_new("expr");
+	mpc_parser_t* Lispy = mpc_new("lispy");
+	
+	mpca_lang(MPCA_LANG_DEFAULT,
+		"								\
+			number 	 : /-?[0-9]+/	;				\
+			operator : '+' | '-' | '*' | '/' ;			\
+			expr     : <number> | '(' <operator> <expr>+ ')' ;	\
+			lispy    : /^/ <operator> <expr>+ /$/ ;			\
+		",
+		Number, Operator, Expr, Lispy);
+		
+		
 	//print version details and exit information
 	puts("Jarvis Version 0.0.1");
 	puts("Press ctrl+c to Exit\n");
@@ -50,6 +66,8 @@ int main(int argc,char **argv)
 		
 		free(input);
 	}
+	
+	mpc_cleanup(4,Number,Operator,Expr,Lispy);
 	
 	return 0;
 }
